2fc41b648ea950b80b94f64ae8f75db0b6181d47
tonykaigai <tonykaigai@gmail.com>
2021-09-14 12:12:12 +0300

    update

--- src/app/list-shows/list-shows.page.ts
+++ src/app/list-shows/list-shows.page.ts
@@ -6,21 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-shows.page.scss'],
 })
 export class ListShowsPage implements OnInit {
-  daBuilding;
+  daBuilding:any;
   CourseLable
 
   constructor() {
     
     this.daBuilding = [
-      { id: 0,avatarimage:'assets/Tomato_je.jpg', prodname: 'Tomato',proddesc: 'Tomato',currentksh: 'Ksh 6000'   },
-      { id: 1,avatarimage:'assets/kales.jpg', prodname: 'Kales',proddesc: 'Kales',currentksh: 'Ksh 1000'     },
-      { id: 2,avatarimage:'assets/red_onions.jpg', prodname: 'Red Onions',proddesc: 'Red Onions',currentksh: 'Ksh 1000 '     },
-      { id: 3,avatarimage:'assets/cabbage.jpg', prodname: 'Cabbage"',proddesc: 'test',currentksh: 'Ksh 2000 '   },
-      { id: 4,avatarimage:'assets/hoho.jpeg', prodname: 'Hoho"',proddesc: 'Hoho',currentksh: 'Ksh 1000 '   },
-      { id: 5,avatarimage:'assets/MweaPishori.png', prodname: 'Mwea Pishori"',proddesc: 'Mwea Pishori',currentksh: 'Ksh 31000 '     },   
+      { id: 0,avatarimage:'assets/Tomato_je.jpg', prodname: 'Tomato',proddesc: 'To receive every Monday 12th',currentksh: 'Ksh 6000' ,scheduledate: '02-02-2021'  },
+      { id: 1,avatarimage:'assets/kales.jpg', prodname: 'Kales',proddesc: 'To receive every 12th day of the month',currentksh: 'Ksh 1000' ,scheduledate: '02-02-2021'    },
+      { id: 2,avatarimage:'assets/red_onions.jpg', prodname: 'Red Onions',proddesc: 'To receive every sunday ',currentksh: 'Ksh 1000 ' ,scheduledate: '02-02-2021'    },
+      { id: 3,avatarimage:'assets/cabbage.jpg', prodname: 'Cabbage"',proddesc: 'To receive every sunday',currentksh: 'Ksh 2000 ' ,scheduledate: '02-02-2021'  },
+      { id: 4,avatarimage:'assets/hoho.jpeg', prodname: 'Hoho"',proddesc: 'To receive every sunday',currentksh: 'Ksh 1000 ' ,scheduledate: '02-02-2021'  },
+      { id: 5,avatarimage:'assets/MweaPishori.png', prodname: 'Mwea Pishori"',proddesc: 'To receive every sunday',currentksh: 'Ksh 31000 '  ,scheduledate: '02-02-2021'   },   
     ];
-
-
    }
 
   ngOnInit() {
